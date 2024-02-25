bfc175deb2aa3e881592616f395cdf65c8433f79
abutun11 <butunalican01@gmail.com>
2024-02-25 13:01:47 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Classes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";
+            customer.LastName = "Demiroğ";
+
+            Customer customer2 = new Customer
+            {
+                Id = 2,
+                City = "Ankara",
+                FirstName = "Derin",
+                LastName = "Demiroğ"
+            };
+        }
+    }
+}
---
+++ Inheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Inheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer(),
+                new Student(),
+                new Person()
+            };
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Customer : Person
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+}
